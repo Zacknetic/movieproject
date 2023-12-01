84949155d3fd4e9e6fa7739c7d094d8bdd35c3c0
Zacknetic <zackavino@gmail.com>
2023-11-30 21:19:58 -0500

    Initial commit

---
+++ src/test/java/com/application/movieproject/MovieprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.movieproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
