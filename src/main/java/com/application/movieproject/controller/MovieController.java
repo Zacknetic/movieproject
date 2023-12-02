c4c62e85d790deaec16a6e86b1dc3fe01510ec5f
Zacknetic <zackavino@gmail.com>
2023-12-01 22:28:46 -0500

    added list-movie.html updated mappings

--- src/main/java/com/application/movieproject/controller/MovieController.java
+++ src/main/java/com/application/movieproject/controller/MovieController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -19,6 +20,20 @@ public class MovieController {
     public String findAllMovies(Model model) {
         List<Movie> movies = movieService.findAllMovies();
         model.addAttribute("movies", movies);
+        return "movies";
+    }
+
+    @GetMapping("/movie/{id}")
+    public String findMovieById(@PathVariable Long id, Model model){
+        Movie movie = movieService.findMovieById(id);
+        model.addAttribute("movie", movie);
+        return "list-movie";
+    }
+
+    @GetMapping("/remove-movie/{id}")
+    public String deleteMovie(@PathVariable Long id, Model model) {
+        movieService.removeMovieById(id);
+        model.addAttribute("movies", movieService.findAllMovies());
         return "movies";
     }
 }
